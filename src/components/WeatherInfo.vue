5756dcb4d2173d448cf27356db86212d7e73f0a4
hilse <trandat.arc@gmail.com>
2021-09-22 22:59:13 +0700

    implement loading before geolocation

--- src/components/WeatherInfo.vue
+++ src/components/WeatherInfo.vue
@@ -1,6 +1,10 @@
 <template>
     <div id='weather-info'>
         <b-container fluid="md" >
+            <div v-if="!gotLocation">
+                <img class="animated-img" src="@/assets/images/compass.svg" alt="compass" width=100xpx>
+                <h2>Getting your localtion...</h2>
+            </div>
             <b-row >
                 <b-col sm="6">
                     <left-panel v-if="currentWeather" v-bind:weatherInfo="currentWeather"></left-panel>
@@ -31,13 +35,15 @@ export default {
     data(){
         return{
             currentWeather: null,
-            coord: {lat:0, lon: 0, isFromGeo: false}
+            coord: {lat:0, lon: 0, isFromGeo: false},
+            gotLocation: false
         }
     },
     created(){
         navigator.geolocation.getCurrentPosition((position) =>{
             axios.get('https://revgeocode.search.hereapi.com/v1/revgeocode?at=' + position.coords.latitude + ',' + position.coords.longitude + '&lang=en-US&apiKey=' + process.env.VUE_APP_HERE_API_KEY)
             .then(response =>{
+                this.gotLocation = true;
                 let cityName = response.data.items[0].address.district + ', ' + response.data.items[0].address.city;
                 this.updateLocation(true, cityName);
                 bus.$emit('locationChanged', {lat: position.coords.latitude, lon: position.coords.longitude, isFromGeo: true});
@@ -47,6 +53,7 @@ export default {
             console.log(error);
             axios.get('https://api.ipdata.co/?api-key=' + process.env.VUE_APP_GEOIP_API_KEY)
             .then(response => {
+                this.gotLocation = true;
                 this.coord.lat = response.data.latitude;
                 this.coord.lon = response.data.longitude;
                 
@@ -95,7 +102,13 @@ export default {
         #weather-info{
             margin-top: 50px;
         }
+        .animate-img{
+            width: 70px;
         
+        }
+        h2{
+            font-size: 1.5em;
+        }
     }
     
 </style>
